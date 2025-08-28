0a0f0e974190a80d5d4405e589d3b5470757461b
Sharnam <sharnam34@gmail.com>
2025-08-28 18:08:23 +0700

    refactor: image path modification

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -4,7 +4,7 @@ export default function Onboarding() {
   return (
     <div className="flex items-center justify-center w-full h-screen bg-slate-100">
       <div className="flex flex-col gap-3 rounded-2xl justify-center max-w-[628px] p-10 bg-white">
-        <img src="/companyLogo.png" alt="Onboarding" className="w-48" />
+        <img src="/form/companyLogo.png" alt="Onboarding" className="w-48" />
         <h1 className="mt-4 text-2xl font-semibold text-slate-700">
           Lets get your brand ready to soar with us
         </h1>
@@ -14,7 +14,7 @@ export default function Onboarding() {
           flows that connect, convert, and boost your revenue
         </p>
         <div className="flex items-center w-full gap-2 p-4 rounded-lg bg-slate-100">
-          <img src="/bulb.png" alt="bulb" className="w-3 " />
+          <img src="/form/bulb.png" alt="bulb" className="w-3 " />
           <p className="text-sm text-slate-500">
             Pro tip: The more accurate your answers, the better your results!
           </p>
